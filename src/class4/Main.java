f134cd222ace98b725bbe31d2b8d0c48724a6290
Ramin Imani <93437949+raminimani@users.noreply.github.com>
2022-01-28 13:32:05 -0500

    here is your car now don't ask me to make changes

--- src/class4/Main.java
+++ src/class4/Main.java
@@ -5,11 +5,10 @@ public class Main {
         //the Basic structure
 
         CarBasics car=new CarBasics();
-
-        car.Color="red";
-        car.Engine="7999cc";
-        car.door="2";
-        car.number="115998";
+        car.Color="green";
+        car.number="789";
+        car.Engine="544444cc";
+        car.door="3";
         System.out.println(car.Color);
 
     }
